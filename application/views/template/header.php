898e1875351d012587368d957f8bc0dd8f0e67d3
Adittya Kamal Mahardin <adittyakamalm@gmail.com>
2021-09-27 10:39:20 +0700

    First Commit

---
+++ application/views/template/header.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+
+    <!-- My CSS-->
+    <link rel="stylesheet" href="<?= base_url('assets/css/styles.css'); ?>">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>">
+
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Crimson+Text&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?= base_url('assets/vendor/font-awesome/css/all.css'); ?>">
+    
+    </head>
+<body>
+    <div class="headers-wrap">
+        <div class="lemdikpol">
+            <img src="assets/images/lemdikpol.png">
+        </div>
+        <div class="polda">
+            <img src="assets/images/polda.png">
+            <div class="spn">
+            <img src="assets/images/spn.png">
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
---
+++ application/views/template/navbar.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" href="<?= base_url('/assets/css/bootstrap.min.css'); ?>">
+<nav class="navbar navbar-light navbar-expand-lg">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav ">
+                <li class="nav-item">
+                    <a class="nav-link mx-2" href="<?= base_url(''); ?>">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link mx-2" href="<?= base_url(''); ?>">Struktur Jabatan</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link  mx-2" href="<?= base_url(''); ?>">Data Peronil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link mx-2" href="<?= base_url(''); ?>">Kontak</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link mx-2" href="<?= base_url(''); ?>">Profil</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
